2384a413b8b55eefa22e69b341760d9ca3359cd2
Marcin Wadoń <mwadon@evojam.com>
2022-10-03 16:38:04 +0200

    v0.1.0

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"dag-cli/infrastructure/config"
-	"dag-cli/infrastructure/lb"
 	"fmt"
 	"os"
 
@@ -14,25 +12,9 @@ var (
 	rootCmd = &cobra.Command{
 		Use:   "dag",
 		Short: "DAG Command Line Utility",
+		Version: "0.1.0",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println("status")
-			cfg, _ := config.LoadConfig()
-
-			fmt.Printf("\nEnter Layer to configure (yes/no) [yes]: ")
-			fmt.Printf("\nEnter L0 load balancer url [https://l0-lb-testnet.constellationnetwork.io]: ")
-			var lay string
-			fmt.Scanln(&lay)
-
-			fmt.Println(lay)
-
-			return nil
-
-			lbClient := lb.GetClient(cfg.L0.LoadBalancer)
-			peer, err := lbClient.GetRandomReadyPeer()
-			if err != nil {
-				return err
-			}
-			fmt.Println(peer.Id)
 
 			return nil
 		},
