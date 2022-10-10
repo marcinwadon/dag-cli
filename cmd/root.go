4ea8d92e03b397ce7c1ae43830d2a9d9afdabca2
Marcin Wadoń <mwadon@evojam.com>
2022-10-10 15:00:56 +0200

    v0.5.0

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ var (
 	rootCmd = &cobra.Command{
 		Use:     "dag",
 		Short:   "DAG Command Line Utility",
-		Version: "v0.4.0",
+		Version: "v0.5.0",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println("status")
 
